e451e0c62c5f3f2e146921acf105b5d9980272f1
Hendrik Klug <hendrik.klug@gmail.com>
2024-07-10 20:44:45 +0200

    Add setup_mytest_repo fixture and refactor cli_runner fixture
    This commit adds a new fixture called `setup_mytest_repo` which clones the `mytest` repository and sets the remote URL using the GitHub token. It also refactors the `cli_runner` fixture to accept the `setup_mytest_repo` fixture as a parameter. These changes improve the test setup and organization.

--- tests/tests.py
+++ tests/tests.py
@@ -11,11 +11,29 @@ from ghchain.git_utils import get_all_branches
 from ghchain.utils import run_command
 
 
+@pytest.fixture(scope="session")
+def setup_mytest_repo(tmpdir_factory):
+    temp_dir = tmpdir_factory.mktemp("shared_temp_dir")
+    os.chdir(temp_dir)
+    logger.info(f"Current working directory: {os.getcwd()}")
+    subprocess.run(
+        ["git", "clone", "https://github.com/HendrikKlug-synthara/mytest.git", "."],
+        capture_output=True,
+        text=True,
+        cwd=os.getcwd(),
+        check=True,
+    )
+
+    assert "GH_TOKEN" in os.environ
+    command = f"git remote set-url origin https://x-access-token:{os.environ['GH_TOKEN']}@github.com/HendrikKlug-synthara/mytest.git"
+    subprocess.run(command, shell=True, check=True)
+    return temp_dir
+
+
 @pytest.fixture
-def cli_runner():
+def cli_runner(setup_mytest_repo):
     runner = CliRunner()
-    with runner.isolated_filesystem():
-        yield runner
+    yield runner, setup_mytest_repo
 
 
 @pytest.fixture(scope="module")
@@ -63,38 +81,20 @@ def create_stack():
 
 @pytest.mark.parametrize("run_workflows", [True, False])
 def test_create_stack(cli_runner, repo_cleanup, run_workflows):
-    with cli_runner.isolated_filesystem():
-        logger.info(f"Current working directory: {os.getcwd()}")
-        logger.info(f"Current directory files: {os.listdir()}")
+    logger.info("Running test_create_stack")
+    logger.info(f"Loaded config from {CONFIG_FN}")
+    logger.info(f"Config: {config.to_dict()}")
+    logger.info(f"Current working directory: {os.getcwd()}")
+    logger.info(f"Current directory files: {os.listdir()}")
+    logger.info(f"Git base dir: {get_git_base_dir()}")
+    create_stack()
 
-        completed_process = subprocess.run(
-            ["git", "clone", "https://github.com/HendrikKlug-synthara/mytest.git", "."],
-            capture_output=True,
-            text=True,
-            cwd=os.getcwd(),
-            check=True,
-        )
+    if run_workflows:
+        result = cli_runner.invoke(cli.process_commits, ["--with-tests"])
+    else:
+        result = cli_runner.invoke(cli.process_commits)
 
-        assert "GH_TOKEN" in os.environ
-        command = f"git remote set-url origin https://x-access-token:{os.environ['GH_TOKEN']}@github.com/HendrikKlug-synthara/mytest.git"
-        subprocess.run(command, shell=True, check=True)
-
-        logger.info(f"Current directory files: {os.listdir()}")
-
-        logger.info("Running test_create_stack")
-        logger.info(f"Loaded config from {CONFIG_FN}")
-        logger.info(f"Config: {config.to_dict()}")
-        logger.info(f"Current working directory: {os.getcwd()}")
-        logger.info(f"Current directory files: {os.listdir()}")
-        logger.info(f"Git base dir: {get_git_base_dir()}")
-        create_stack()
-
-        if run_workflows:
-            result = cli_runner.invoke(cli.process_commits, ["--with-tests"])
-        else:
-            result = cli_runner.invoke(cli.process_commits)
-
-        assert result.exit_code == 0
+    assert result.exit_code == 0
 
 
 if __name__ == "__main__":
