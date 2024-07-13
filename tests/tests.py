9ed28cd259d420c0bf13cd54b8e849db3ee1b0c2
Hendrik Klug <hendrik.klug@gmail.com>
2024-07-13 12:05:07 +0200

    add github ci tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,55 +1,125 @@
-from unittest.mock import patch, MagicMock
+import json
+import os
+import subprocess
+
+import pytest
 from click.testing import CliRunner
+
 from ghchain import cli
+from ghchain.config import logger
+from ghchain.git_utils import get_all_branches
+from ghchain.stack import Stack
+from ghchain.utils import run_command
 
 
-def test_main_rebase_onto():
-    runner = CliRunner()
-    with patch("ghchain.cli.rebase_onto_branch") as mock_rebase:
-        result = runner.invoke(cli.main, ["--rebase-onto", "branch"])
-        mock_rebase.assert_called_once_with("branch")
-        assert result.exit_code == 0
+@pytest.fixture(scope="session")
+def setup_mytest_repo(tmpdir_factory):
+    temp_dir = tmpdir_factory.mktemp("shared_temp_dir")
+    os.chdir(temp_dir)
+    logger.info(f"Current working directory: {os.getcwd()}")
+    subprocess.run(
+        ["git", "clone", "https://github.com/HendrikKlug-synthara/mytest.git", "."],
+        capture_output=True,
+        text=True,
+        cwd=os.getcwd(),
+        check=True,
+    )
 
-
-def test_main_status():
-    runner = CliRunner()
-    with patch("ghchain.cli.print_status") as mock_status:
-        result = runner.invoke(cli.main, ["--status"])
-        mock_status.assert_called_once()
-        assert result.exit_code == 0
-
-
-def test_main_live_status():
-    runner = CliRunner()
-    with patch("ghchain.cli.print_status") as mock_status:
-        result = runner.invoke(cli.main, ["--live-status"])
-        mock_status.assert_called_once_with(base_branch="main", live=True)
-        assert result.exit_code == 0
-
-
-def test_main_run_tests():
-    runner = CliRunner()
-    with patch("ghchain.cli.get_branch_name_for_pr_id") as mock_get_branch, patch(
-        "ghchain.cli.get_current_branch"
-    ) as mock_get_current, patch(
-        "ghchain.cli.get_pr_url_for_branch"
-    ) as mock_get_pr_url, patch("ghchain.cli.update_pr_descriptions") as mock_update_pr:
-        mock_get_branch.return_value = "branch"
-        mock_get_current.return_value = "current_branch"
-        mock_get_pr_url.return_value = "pr_url"
-        result = runner.invoke(cli.main, ["--run-tests", "1"])
-        mock_get_branch.assert_called_once_with(1)
-        mock_get_pr_url.assert_called_once_with("branch")
-        mock_update_pr.assert_called_once_with(
-            run_tests=("pr_url", "branch"), pr_stack=["pr_url"]
+    if "GH_TOKEN" in os.environ:
+        command = (
+            f"git remote set-url origin "
+            f"https://x-access-token:{os.environ['GH_TOKEN']}@"
+            f"github.com/HendrikKlug-synthara/mytest.git"
         )
-        assert result.exit_code == 0
+        subprocess.run(command, shell=True, check=True)
+    return temp_dir
 
 
-def test_main_no_commits():
+@pytest.fixture
+def cli_runner(setup_mytest_repo):
     runner = CliRunner()
-    with patch("ghchain.cli.Stack.create") as mock_stack:
-        mock_stack.return_value = MagicMock(commits=None)
-        result = runner.invoke(cli.main)
-        assert "No commits found that are not in main." in result.output
-        assert result.exit_code == 0
+    yield runner, setup_mytest_repo
+
+
+@pytest.fixture(scope="function")
+def repo_cleanup():
+    """Fixture to clean up the repository before tests."""
+    cleanup_repo()
+    yield
+
+
+def cleanup_repo():
+    """Delete all current branches and pull requests and create a new branch mydev."""
+    run_command(["git", "checkout", "main"])
+    run_command(["git", "reset", "--hard", "464397e0bf88c66ea09fd05448ad6847c230c2fe"])
+    run_command(["git", "push", "-f"])
+
+    branches = set(get_all_branches()) - {"main"}
+
+    for branch in branches:
+        run_command(["git", "branch", "-D", branch])
+        run_command(["git", "push", "origin", "--delete", branch])
+
+    prs_json = run_command(
+        ["gh", "pr", "list", "--json", "url"],
+    ).stdout
+
+    prs = json.loads(prs_json)
+    for pr in prs:
+        pr_url = pr["url"]
+        run_command(
+            ["gh", "pr", "close", pr_url, "-d"],
+        )
+
+    run_command(["git", "checkout", "main"])
+    run_command(["git", "checkout", "-b", "mydev"])
+
+
+def create_stack():
+    """Change the README.md file 4 times and commit each change."""
+    for i in range(4):
+        with open("README.md", "a") as f:
+            f.write(f"line {i}\n")
+        run_command(["git", "add", "README.md"])
+        run_command(["git", "commit", "-m", f"commit {i}"])
+
+
+@pytest.mark.order(1)
+@pytest.mark.parametrize("run_workflows", [False, True])
+def test_process_commits(cli_runner, repo_cleanup, run_workflows):
+    cli_runner, _ = cli_runner
+
+    create_stack()
+
+    if run_workflows:
+        result = cli_runner.invoke(cli.process_commits, ["--with-tests"])
+    else:
+        result = cli_runner.invoke(cli.process_commits)
+
+    # assert that the repo has 4 open pull requests
+    prs = run_command(["gh", "pr", "list", "--json", "url"]).stdout
+    prs = json.loads(prs)
+    assert len(prs) == 4, f"Expected 4 pull requests, got {len(prs)}"
+
+    assert result.exit_code == 0
+
+
+@pytest.mark.order(2)
+def test_land(cli_runner):
+    cli_runner, _ = cli_runner
+
+    stack = Stack.create(base_branch="main")
+    branch_to_land = stack.branches[-1]
+    runner = CliRunner()
+    result = runner.invoke(cli.land, ["-b", branch_to_land])
+
+    # assert that the repo has 3 open pull requests
+    prs = run_command(["gh", "pr", "list", "--json", "url"]).stdout
+    prs = json.loads(prs)
+    assert len(prs) == 3, f"Expected 3 pull requests, got {len(prs)}"
+
+    assert result.exit_code == 0
+
+
+if __name__ == "__main__":
+    pytest.main(["-v", __file__, "-s"])
